aac7b767dfc0d76cb0de6e9de8b0bf5a2efbab39
marsupialmarcos <dasilva.marcos@gmail.com>
2019-03-12 13:51:06 -0700

    refactor the readAll function

--- datastore/index.js
+++ datastore/index.js
@@ -26,51 +26,40 @@ exports.create = (text, callback) => {
     });
   });
 };
-
-
-//i: todo text string, callback that creates a new .txt file named after the id# and saves todo text string in it
-  //callback function 
-    // i: err, results
-    // o: new file with todo text saved in it
-    // code:
-      // (err, res) => {
-      //    create a file path
-      //    writeFile to add a value to the filepath 
-      // }
-//o: new todo with a corresponding file that saves its value
-// getNextUniqueID callback (err, res) => {
-  //   res.writeHeader(200, headers)
-  //   res.write( countString );
-  //   res.end();  
-  // }
-
-
-  
-  
-
-
-
-//fs.writeFile to create a new file
-//exports.createFile = path.join(__dirname, 'counter.txt');
-  //the directory will be ./data  
-  //file path name will be the id.txt `${id}.txt`
-
-
-  // = (err, res) => {
-  
-  //   exports.createFile = path.join('./data', `${id}.txt`);
-  //   // var filepath = `./data/${id}.txt`;
-  //   fs.writeFile(exports.createFile, items[id], (err, text) => {
-  //     console.log(item[id]);
-  //   })
-  // }
   
 
 exports.readAll = (callback) => {
-  var data = _.map(items, (text, id) => {
-    return { id, text };
-  });
-  callback(null, data);
+  // var data = _.map(items, (text, id) => {
+  //   return { id, text };
+  // });
+  fs.readdir('./test/testData', (err, data) => {
+    if (err) {
+      console.log(`ERROR: ${err}`);
+      callback(err);
+    } else {
+      var removeTxt = _.map(data, (fileName) => {fileName = str.slice(5, 4);})
+      var mappedData = _.map(removeTxt, (value) => ({id: value, text: value}));
+      callback(null, mappedData);
+    }
+  })
+  
+
+  // i: callback function
+  // o: array of todos (that we'll send to the client via GET request)
+  // c: do not attempt to read the contents of each file that contains todo item text- DON'T DO IT!
+    // must include a text field in your response to the client, use the message's id for both the id field and the text field 
+  // e: no todos should return an empty array
+
+  //justification: to create a route to the collection of todos. Create a way to access all todos
+  //explanation: readAll accepts a callback, maps the data to key,value pairs, and then runs a callback on those key value pairs. at some point, we need to put the data in an array
+
+  //pseudo code
+  //  1. use fs.readdir to access the contents of the directory and put them into an array
+      // console.log the data to see what it looks like
+  //  2. map over the list, and create an object where id and text are both set equal to the file name string
+  //regex to remove .txt from the key, value pairs
+
+
 };
 
 exports.readOne = (id, callback) => {
@@ -112,3 +101,16 @@ exports.initialize = () => {
     fs.mkdirSync(exports.dataDir);
   }
 };
+
+fs.readdir('./test/testData', (err, data) => {
+  if (err) {
+    console.log(`ERROR: ${err}`);
+    // callback(err);
+  } else {
+    var removeTxt = _.map(data, (fileName) => {return fileName.slice(0, 5);});
+    console.log(removeTxt);
+    // var mappedData = _.map(data, (value) => ({id: value, text: value}));
+    // callback(null, mappedData);
+    // console.log(mappedData);
+  }
+})
