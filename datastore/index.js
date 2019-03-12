692845cdd532d26a0cc53c92a931a127f244f065
marsupialmarcos <dasilva.marcos@gmail.com>
2019-03-12 15:32:44 -0700

    refactor the readOne method
    - refactor the readOne to read a todo item from the dataDir based on the message's id
    - use readFile to find the file path and get the contents

--- datastore/index.js
+++ datastore/index.js
@@ -38,38 +38,41 @@ exports.readAll = (callback) => {
       callback(err);
     } else {
       var mappedData = _.map(data, (value) => ({id: value.slice(0, 5), text: value.slice(0, 5)}));
-      console.log(mappedData);
+      // console.log(mappedData);
       callback(null, mappedData);
     }
   });
-  
-
-  // i: callback function
-  // o: array of todos (that we'll send to the client via GET request)
-  // c: do not attempt to read the contents of each file that contains todo item text- DON'T DO IT!
-  //  must include a text field in your response to the client, use the message's id for both the id field and the text field 
-  // e: no todos should return an empty array
-
-  //justification: to create a route to the collection of todos. Create a way to access all todos
-  //explanation: readAll accepts a callback, maps the data to key,value pairs, and then runs a callback on those key value pairs. at some point, we need to put the data in an array
-
-  //pseudo code
-  //  1. use fs.readdir to access the contents of the directory and put them into an array
-  //  console.log the data to see what it looks like
-  //  2. map over the list, and create an object where id and text are both set equal to the file name string
-  //regex to remove .txt from the key, value pairs
-
-
 };
 
 exports.readOne = (id, callback) => {
-  var text = items[id];
-  if (!text) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback(null, { id, text });
-  }
-};
+  //Read exports.dataDir for the fileName containing the id (callback)
+  //  callback success case will read the file `${id}.txt`
+  //    callback successs of readFile will use the readOne callback with the parameter {id: id, text: data}
+  // var text = items[id];
+  // if (!text) {
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback(null, { id, text });
+  // }
+
+  fs.readFile(`${exports.dataDir}/${id}.txt`, (err, fileContents) => {
+    if (err) {
+      console.log(`ERROR: ${err}`);
+      callback(err);
+    } else {
+      callback(null, {id: id, text: fileContents});
+    }
+  })
+};    
+
+//i: id string and callback funtion (err, res) => {}
+//o: will read 1 todo item from the dataDir and do something with it via callback
+//c: only read one at a time, 
+//e: 
+//justification: to read one todo item that can be returned when a GET request is made
+//explanation: the function takes the id and a callback, finds the corresponding file in _.dataDir and reads it. if it reads successfully, callback on the data
+
+
 
 exports.update = (id, text, callback) => {
   var item = items[id];
